11595cf12faf6f7344432da9b49dad21c48183f9
mongzera <40034319+mongzera@users.noreply.github.com>
2025-07-03 17:06:29 +0800

    Fixed route and removed -> sign on every page

--- src/dashboard/CurrentView.tsx
+++ src/dashboard/CurrentView.tsx
@@ -1,13 +1,12 @@
 import './CurrentView.css'
 
 function CurrentView(props : any){
-    console.log(props);
 
     if(props.displayView === undefined) return "Are you trying to hack me?";
 
     return (
         <div className="current-view">
-            <h4 className='weight-600'> <i className='bi bi-arrow-right mx-2'/>{props.displayName}</h4>
+            <h4 className='weight-600'>{props.displayName}</h4>
             {props.displayView}
         </div>
     )
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Dashboard from './dashboard/Dashboard.tsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Login from './auth/login/Login.tsx'
@@ -13,7 +13,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-
+          <Route path='/' element={<Navigate to={'/login'}/>}/>
           <Route path='/login' element={<Login />}/>
           <Route path='/create-account' element={<CreateAccount />}/>
           
